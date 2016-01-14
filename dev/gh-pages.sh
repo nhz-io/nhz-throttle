f10c4c5c19f7af6f7db7b0a26e4ddead15120f44
Ishi Ruy <dev@nhz.io>
2016-01-14 03:48:51 +0200

    hide output

--- dev/gh-pages.sh
+++ dev/gh-pages.sh
@@ -15,4 +15,4 @@ git config --local user.email "dev@nhz.io"
 git add . --all
 git commit -m "Deploy to GitHub Pages"
 
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages 
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
